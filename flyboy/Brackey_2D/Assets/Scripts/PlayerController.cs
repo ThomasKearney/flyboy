9c4fc552dcdad9f9d3f1d02b2e388d5622ca6bf1
Thomas Kearney <thomasnkearney@gmail.com>
2019-06-24 20:05:07 -0500

    Initial commit

---
+++ flyboy/Brackey_2D/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // BASIC
+    public float speedInput;
+    public Animator animator;
+    //
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+
+    private Rigidbody2D rigidBody;
+
+    // FLIP
+    private bool facingRight = true;
+
+    // GROUNDING
+    public bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    // SOFT FALL
+    public float softFallDrag;
+    public float softFallHorizontalDrag;
+
+
+    private void Start()
+    {
+        speed = speedInput;
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Jumping() && isGrounded == true)
+        {
+            rigidBody.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        SoftLandController();
+
+        // Right arrow =  1
+        // Left arrow  = -1
+        moveInput = Input.GetAxis("Horizontal");
+        rigidBody.velocity = new Vector2(moveInput * speed, rigidBody.velocity.y);
+
+        if(facingRight == false && moveInput > 0)
+        {
+            Flip();
+        } else if(facingRight == true && moveInput < 0) {
+            Flip();
+        }
+
+    }
+
+    private void SoftLandController()
+    {
+        // If we are falling, not on the ground, and are holding a jump button then increase our drag
+        if(rigidBody.velocity.y < 0 && !isGrounded && (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Space)))
+        {
+            rigidBody.drag = softFallDrag;
+            speed = speedInput / softFallHorizontalDrag;
+            animator.SetBool("slowFall", true);
+        }
+        else
+        {
+            rigidBody.drag = 0;
+            speed = speedInput;
+            animator.SetBool("slowFall", false);
+        }
+
+    }
+
+    private bool Jumping()
+    {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)) {return true;}
+        else { return false; }
+    }
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 playerLocalScaler = transform.localScale;
+        playerLocalScaler.x *= -1;
+        transform.localScale = playerLocalScaler;
+    }
+
+}
